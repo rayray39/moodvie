9623b6794910a4542720babdaa5e89b608c0dabf
rayray39 <raynerlimfy@gmail.com>
2025-05-31 21:22:20 +0800

    Pages.tsx: Customize tabs

--- src/components/Pages.tsx
+++ src/components/Pages.tsx
@@ -4,33 +4,33 @@ import Movies from "./Movies";
 
 function Pages() {
     return (
-        <Tabs variant="outline" defaultValue="home">
+        <Tabs variant="outline" defaultValue="home" mt={'10'}>
 
-        <Tabs.List>
-            <Tabs.Tab value="home" >
-                Home
-            </Tabs.Tab>
-            <Tabs.Tab value="favourites" >
-                Favourites
-            </Tabs.Tab>
-            <Tabs.Tab value="watched" >
-                Watched
-            </Tabs.Tab>
-        </Tabs.List>
+            <Tabs.List justify="center" grow>
+                <Tabs.Tab value="home" fw={700}>
+                    Home
+                </Tabs.Tab>
+                <Tabs.Tab value="favourites" fw={700} >
+                    Favourites
+                </Tabs.Tab>
+                <Tabs.Tab value="watched" fw={700} >
+                    Watched
+                </Tabs.Tab>
+            </Tabs.List>
 
-        <Tabs.Panel value="home">
-            <MovieProvider>
-                <Movies />
-            </MovieProvider>
-        </Tabs.Panel>
+            <Tabs.Panel value="home">
+                <MovieProvider>
+                    <Movies />
+                </MovieProvider>
+            </Tabs.Panel>
 
-        <Tabs.Panel value="favourites">
-            Favourites tab content
-        </Tabs.Panel>
+            <Tabs.Panel value="favourites">
+                Favourites tab content
+            </Tabs.Panel>
 
-        <Tabs.Panel value="watched">
-            Watched tab content
-        </Tabs.Panel>
+            <Tabs.Panel value="watched">
+                Watched tab content
+            </Tabs.Panel>
         </Tabs>
     );
 }
