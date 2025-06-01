9ff3fe3e53e7d5a8aacaca3744864924b34bd73d
rayray39 <raynerlimfy@gmail.com>
2025-06-01 13:14:08 +0800

    Favourites.tsx: Add get request to fetch favourite movies

--- src/components/Favourites.tsx
+++ src/components/Favourites.tsx
@@ -6,16 +6,33 @@ import MovieCard from "./MovieCard";
 function Favourites() {
     const [favMovies, setFavMovies] = useState<Movie[]>([]);
 
+    const fetchFavourites = async () => {
+        try {
+            const response = await fetch('http://localhost:5000/get-favourites', {
+                method:'GET',
+                headers:{'Content-Type':'application/json'}
+            })
+
+            const data = await response.json();
+            console.log(data.message);
+            setFavMovies(data.results);
+        } catch (error) {
+            console.log('Error occured while fetching favourite movies:', error);
+            return;
+        }
+    }
+
     useEffect(() => {
         // fetch all the fav movies from the favourites.JSON file
+        fetchFavourites();
     }, [])
 
     return <>
-        <Stack style={{
-            width:'600px'
+        <Stack mt={'sm'} style={{
+            width:'600px',
         }}>
             {
-                favMovies.map(movie => (
+                favMovies && favMovies.map(movie => (
                     <MovieCard
                         key={movie.id}
                         movieId={movie.id}
