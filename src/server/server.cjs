d92162ea38f595ccc510303396e8d4fc99210afb
rayray39 <raynerlimfy@gmail.com>
2025-06-01 13:12:44 +0800

    server: Set up express server and endpoints for favourites

---
+++ src/server/server.cjs
@@ -0,0 +1,68 @@
+const express = require('express');
+const cors = require('cors');
+const fs = require('fs');
+const path = require('path');
+
+// Set up Express app and port number
+const app = express();
+const PORT = 5000;
+
+// Define middlware
+app.use(cors());
+app.use(express.json());
+
+const FAVOURITES_FILE = path.join(__dirname, '../data/', 'favourites.json');
+
+// Helper to read favourites from JSON file
+function readFavourites() {
+    return new Promise((resolve, reject) => {
+        fs.readFile(FAVOURITES_FILE, 'utf-8', (err, data) => {
+            if (err) reject(err);
+            else resolve(JSON.parse(data));
+        });
+    });
+}
+
+// Helper to write favourites to JSON file
+function writeFavourites(movies) {
+    return new Promise((resolve, reject) => {
+        fs.writeFile(FAVOURITES_FILE, JSON.stringify(movies, null, 2), (err) => {
+            if (err) reject(err);
+            else resolve();
+        });
+    });
+}
+
+// returns all the movies in the favourites.json file
+app.get('/get-favourites', async (req, res) => {
+    try {
+        let favs = await readFavourites();
+        favs = favs.filter(movie => movie && typeof movie === 'object')
+        return res.status(200).json({ results: favs, message: 'Successfully fetched all fav movies from JSON.' });
+    } catch (error) {
+        return res.status(500).json({ message: 'Error: Failed to fetch fav movies from JSON file.' });
+    }
+})
+
+// adds a new movie to the favourites.json file
+app.post('/add-favourites', async (req, res) => {
+    try {
+        const newMovie = req.body;
+        const favs = await readFavourites();
+
+        // Avoid duplicates
+        if (favs.find((movie) => movie.id === newMovie.id)) {
+            return res.status(400).json({ error: 'Movie already in favourites.' });
+        }
+
+        favs.push(newMovie);
+        await writeFavourites(favs);
+        return res.status(200).json({ messsage: 'Successfully added movie to favourites.JSON.' });
+    } catch (error) {
+        return res.status(500).json({ message: 'Error: Failed to add movie to favourites JSON file.' });
+    }
+})
+
+app.listen(PORT, () => {
+    console.log(`Server running at http://localhost:${PORT}`);
+});
