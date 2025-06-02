a9e98bd6f546f8f90a3209e5a85347f1971e9093
rayray39 <raynerlimfy@gmail.com>
2025-06-02 11:08:18 +0800

    Watched.tsx: Set up watched page by using watched context

--- src/App.tsx
+++ src/App.tsx
@@ -3,26 +3,29 @@ import Pages from "./components/Pages"
 import SearchBar from "./components/SearchBar"
 import { MovieProvider } from "./context/MovieContext"
 import { FavouritesProvider } from "./context/FavouritesContext"
+import { WatchedProvider } from "./context/WatchedContext"
 
 function App() {
 
   return (
     <MovieProvider>
-        <FavouritesProvider>
-                <Stack gap={'xs'} style={{
-                    display:'flex',
-                    justifyContent:'start',
-                    alignItems:'center',
-                    height:'100vh'
-                }}>
-                <Text size="xl" fw={700} tt={'uppercase'}>Moodvie</Text>
-                <Text>A movie recommender system based on your mood.</Text>
-                
-                <SearchBar />    
+    <FavouritesProvider>
+    <WatchedProvider>
+            <Stack gap={'xs'} style={{
+                display:'flex',
+                justifyContent:'start',
+                alignItems:'center',
+                height:'100vh'
+            }}>
+            <Text size="xl" fw={700} tt={'uppercase'}>Moodvie</Text>
+            <Text>A movie recommender system based on your mood.</Text>
+            
+            <SearchBar />    
 
-                <Pages />
-            </Stack>
-        </FavouritesProvider>
+            <Pages />
+        </Stack>
+    </WatchedProvider>
+    </FavouritesProvider>
     </MovieProvider>
   )
 }
