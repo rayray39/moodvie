6d8952c584ba3ccf246c60369697bdffe9c95c2c
rayray39 <raynerlimfy@gmail.com>
2025-06-02 15:26:39 +0800

    WatchedContext.tsx: Add requests from client to backend server

--- src/context/WatchedContext.tsx
+++ src/context/WatchedContext.tsx
@@ -15,14 +15,52 @@ export const WatchedProvider: React.FC<{ children: React.ReactNode }> = ({ child
 
     const addToWatched = async (movie: Movie) => {
         // adds the movie to the watched list
+        try {
+            const respoonse = await fetch('http://localhost:5000/add-watched', {
+                method:'POST',
+                headers:{'Content-Type':'application/json'},
+                body:JSON.stringify(movie)
+            })
+
+            const data = await respoonse.json();
+            console.log(data.message);
+            setWatched(data.results);
+            console.log(`movie added to watched list: ${movie.id}`);
+        } catch (error) {
+            console.log('Error occured while adding watched movie:', error);
+        }
     }
 
     const fetchFromWatched = async () => {
         // returns all movies in the watched list
+        try {
+            const respoonse = await fetch('http://localhost:5000/get-watched', {
+                method:'GET',
+                headers:{'Content-Type':'application/json'},
+            })
+
+            const data = await respoonse.json();
+            console.log(data.message);
+            setWatched(data.results);
+        } catch (error) {
+            console.log('Error occured while fetching watched movies:', error);
+        }
     }
 
     const removeFromWatched = async (id: number) => {
         // removes the movie from the watched list
+        try {
+            const respoonse = await fetch(`http://localhost:5000/remove-watched/${id}`, {
+                method:'DELETE',
+                headers:{'Content-Type':'application/json'},
+            })
+
+            const data = await respoonse.json();
+            console.log(data.message);
+            setWatched(data.results);
+        } catch (error) {
+            console.log('Error occured while removing watched movie:', error);
+        }
     }
 
     return (
